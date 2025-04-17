bb71ab48923c3eb87715029bcb7d519cdb01e104
Tomasz Wrona <tomasz.wrona@hotmail.com>
2025-04-17 16:15:12 +0200

    Fix build

--- router/router.go
+++ router/router.go
@@ -63,7 +63,7 @@ func Configure(m *wserver.Manager, client remote.Client) *gin.Engine {
 	protected.POST("/api/servers", postCreateServer)
 	protected.DELETE("/api/transfers/:server", deleteTransfer)
 
-	router.GET("/api/node/stats", getNodeStats)
+	router.GET("/api/node/stats", GetNodeStats)
 
 	// These are server specific routes, and require that the request be authorized, and
 	// that the server exist on the Daemon.
